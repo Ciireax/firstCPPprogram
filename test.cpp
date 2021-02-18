bde7c1cc88c43d2e0500cc1056a43a7002be2d5f
Ciireax <78732577+Ciireax@users.noreply.github.com>
2021-02-18 22:01:16 +0000

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello World" << endl;
+	cout << "Press Enter to exit" << endl;
+	
+	getchar();
+		
+	return 0;
+
+
+}
